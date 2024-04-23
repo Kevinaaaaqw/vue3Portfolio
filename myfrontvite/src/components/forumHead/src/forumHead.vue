f54d13f432831cf06c95e1ed150466fb7747e2c8
kevin <kevin19960908@gmail.com>
2024-04-23 11:49:00 +0800

    113/04/23   論壇頁頭功能完成

--- myfrontvite/src/components/forumHead/src/forumHead.vue
+++ myfrontvite/src/components/forumHead/src/forumHead.vue
@@ -1,34 +1,40 @@
 <script setup lang="ts">
 import { UI1input } from '@/components/UI1'
+import { useRouter } from 'vue-router'
 import { ref } from 'vue'
 
+const router = useRouter()
 const searchInput = ref<string>('')
 
 const toSearchPage = (path: string) => {
-    console.log(path)
+    path && router.push('/forum/search?q=' + path)
+}
+
+const toCategoryPage = () => {
+    router.currentRoute.value.path === '/forum' ? router.go(0) : router.push('/forum')
+}
+
+const toAllPage = () => {
+    router.currentRoute.value.path === '/forum/all' ? router.go(0) : router.push('/forum/all')
 }
 
 </script>
 <template>
     <div class="flex justify-between items-center">
         <div class="flex children:ms-2 children:me-2 children:cursor-pointer hover:children:text-sub">
-            <div>
+            <div @click="() => { toCategoryPage() }">
                 <h3>分類</h3>
             </div>
-            <div>
+            <div @click="() => { toAllPage() }">
                 <h3>全部類別</h3>
             </div>
         </div>
         <div class="w-300px relative">
-            <UI1input :keydown-enter="() => { toSearchPage(searchInput) }" v-model="searchInput" :title="'搜尋'" name="search"
-                id="search" />
+            <UI1input :keydown-enter="() => { toSearchPage(searchInput) }" v-model="searchInput" :title="'搜尋'"
+                name="search" id="search" />
             <img class="absolute cursor-pointer right-0 top-50% right-10px translate-y--50%"
                 src="@/components/icons/search.png" alt="404" @click="() => { toSearchPage(searchInput) }">
         </div>
     </div>
+    <div class="border-b-1px border-solid border-bg/50 mb-3"></div>
 </template>
-<style>
-.forum-head-line::after {
-    contain: content;
-}
-</style>
