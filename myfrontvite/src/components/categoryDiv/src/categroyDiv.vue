1aa940effd638cf9b6be75a96d0382127aa7c105
kevin <128761880+Kevinaaaaqw@users.noreply.github.com>
2024-04-26 09:57:51 +0800

    113/04/26  完成分類介紹元件

--- myfrontvite/src/components/categoryDiv/src/categroyDiv.vue
+++ myfrontvite/src/components/categoryDiv/src/categroyDiv.vue
@@ -1,19 +1,27 @@
 <script setup lang="ts">
+import { ref } from 'vue'
 const props = defineProps<{ img?: string, categoryName?: string, introdece?: string, src?: string }>()
+
+console.log(props.introdece)
+console.log(props.introdece?.length > 200)
+const text = ref()
 
 </script>
 <template>
-    <div class="w-450px h-450px m-3 border-solid border-1px border-base rd-1 cursor-pointer">
+    <div :title="props.introdece"
+        class="w-450px h-450px m-3 border-solid border-1px border-base rd-1 cursor-pointer hover:border-2px hover:border-sub box-border overflow-hidden">
         <div class="w-full h-270px bg-sub/10">
             <img v-if="props.src" :src="props.src" alt="" class="w-full h-full object-contain " loading="lazy">
         </div>
         <div class="border-1px border-b-solid border-base"></div>
-        <div class="w-full h-180px">
-            <div class="ms-2 me-2">
+        <div class="w-full h-180px overflow-hidden text-ellipsis children:ms-2 children:me-2">
+            <div class="">
                 <h2>{{ props.categoryName }}</h2>
             </div>
-            <div class=" ms-2 me-2">
-                <h3>{{ props.introdece }}</h3>
+            <div class="">
+                <h3 class="overflow-hidden line-clamp-4 text-ellipsis">
+                    {{ props.introdece }}
+                </h3>
             </div>
         </div>
     </div>
