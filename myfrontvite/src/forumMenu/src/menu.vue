798b4b93cc751e5dba7c1f24cb88df46f2e4d92d
kevin <128761880+Kevinaaaaqw@users.noreply.github.com>
2024-04-29 15:58:48 +0800

    修正 返回首頁的路徑

--- myfrontvite/src/forumMenu/src/menu.vue
+++ myfrontvite/src/forumMenu/src/menu.vue
@@ -22,10 +22,10 @@ const logStatus = computed(() => useMember().logStatus)
 <template>
   <div class="w-100% flex justify-center bg-subbg">
     <div class="w-full justify-between flex max-w-1000px">
-      <img class="cursor-pointer" src="@/components/icons/logo2.png" alt="載入失敗" @click="() => { toPage('./') }">
+      <img class="cursor-pointer" src="@/components/icons/logo2.png" alt="載入失敗" @click="() => { toPage('/') }">
       <div
         class="flex items-center children:cursor-pointer children:ps-3 children:pe-3 hover:children:bg-black/20 hover:children:duration-500">
-        <div @click="() => { toPage('./') }">
+        <div @click="() => { toPage('/') }">
           <h3>首頁</h3>
         </div>
         <div @click="() => { toForum() }">
