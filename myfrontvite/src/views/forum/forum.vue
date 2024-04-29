d3d5ec694428ded48397cb914a7efadedce37d96
kevin <128761880+Kevinaaaaqw@users.noreply.github.com>
2024-04-29 15:11:34 +0800

    113/04/29 完成論壇文章分類列表頁面

--- myfrontvite/src/views/forum/forum.vue
+++ myfrontvite/src/views/forum/forum.vue
@@ -5,29 +5,60 @@ import { CategroyDiv } from '@/components/categoryDiv'
 import { forumCategoryApi } from '@/api/forum'
 import { useRouter } from 'vue-router'
 import { ref } from 'vue';
+import { ArticleComponent } from '@/components/articleComponent'
 
-const { getCategory } = forumCategoryApi
-
-getCategory().then((item) => {
-    categoryList.value = item.data.categoryList
-    console.log(item)
-})
+const { getCategory, getRecommendedAarticle } = forumCategoryApi
 
 const categoryList = ref<[{ article_category: number; article_category_name: string; article_category_img: string; article_category_introduce?: string }?]>([])
+const recommendedArticleList = ref<{
+    article_category: string, forum_article_id: string,
+    forum_reply_id: string,
+    forum_item_title: string,
+    account: string,
+    forum_article_content: string,
+    creat_time: string,
+    comment: string
+}[]>([])
 
+getCategory().then((item) => {
+    Array.isArray(item.data.categoryList) && (categoryList.value = item.data.categoryList)
+})
+
+getRecommendedAarticle().then((item) => {
+    Array.isArray(recommendedArticleList.value) && (recommendedArticleList.value = item.data.articleList)
+})
 
 const router = useRouter()
-const toCategory = (path?: string | number) => {
-    router.push('/forum/' + path?.toString())
+// 轉文章分類頁面
+const toArticleCategory = (path?: string | number) => {
+    router.push('/forum/article?ac=' + path?.toString())
+}
+
+// 轉文章分類頁面
+const toWritings = (ac: string | number, ai: string | number) => {
+    router.push(`/forum/article?ac=${ac?.toString()}&ai=${ai.toString()}`)
 }
 
 </script>
 <template>
-    <ForumHead />
-    <Slogan />
-    <div class="grid grid-cols-2 justify-center">
-        <CategroyDiv v-for="(i) in categoryList" :src="i?.article_category_img" alt="404"
-            :categoryName="i?.article_category_name" :introdece="i?.article_category_introduce" ulr="/aaa"
-            @click="() => { toCategory(i?.article_category) }" />
+    <div class="p-2">
+        <ForumHead />
+        <Slogan />
+        <div class="grid justify-center mb-5 lg:grid-cols-2">
+            <CategroyDiv v-for="(i) in categoryList" :src="i?.article_category_img" alt="404"
+                :categoryName="i?.article_category_name" :introdece="i?.article_category_introduce" ulr="/aaa"
+                @click="() => { toArticleCategory(i?.article_category) }" />
+        </div>
+        <div class="text-base mb-5">
+            <h2>最新文章</h2>
+        </div>
+        <div class="grid grid-cols-3 mb-5">
+            <ArticleComponent @click="() => { toWritings(i.article_category, i.forum_article_id) }"
+                v-for="(i) in recommendedArticleList" :article_category="i?.article_category"
+                :forum_article_id="i?.forum_article_id" :forum_reply_id="i?.forum_reply_id"
+                :forum_item_title="i?.forum_item_title" :account="i?.account"
+                :forum_article_content="i?.forum_article_content" :creat_time="i?.creat_time" :comment="i?.comment" />
+        </div>
     </div>
+
 </template>
