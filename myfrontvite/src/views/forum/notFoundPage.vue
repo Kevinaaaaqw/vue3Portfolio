780f286aea91f897c8f430cf00576bbc3ae56108
kevin <128761880+Kevinaaaaqw@users.noreply.github.com>
2024-05-02 11:41:35 +0800

    完成出錯(404) 頁面

--- myfrontvite/src/views/forum/notFoundPage.vue
+++ myfrontvite/src/views/forum/notFoundPage.vue
@@ -11,5 +11,9 @@ import { inject, ref, watch } from 'vue'
 // returnPage()
 </script>
 <template>
-    404
+    <div class="flex justify-center w-full h-500px items-center">
+        <span class="text-10 font-extrabold">
+            很抱歉，頁面出現一些錯誤｡ﾟヽ(ﾟ´Д`)ﾉﾟ｡
+        </span>
+    </div>
 </template>
