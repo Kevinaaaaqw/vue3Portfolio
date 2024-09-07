326dba82903772334daf1885aca3c112a1856c73
kevin <kevin19960908@gmail.com>
2024-09-07 14:14:00 +0800

    引入 icon

--- myfrontvite/src/main.ts
+++ myfrontvite/src/main.ts
@@ -5,8 +5,10 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
+import '@fortawesome/fontawesome-free/css/all.css';
 import 'uno.css'
 import VueCookie from 'vue3-cookies'
+
 // async function prepareApp() {
 
 //     const { worker } = await import('./mock/broswer')
