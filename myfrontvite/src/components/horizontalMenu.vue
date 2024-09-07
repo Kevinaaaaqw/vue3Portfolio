8bdfa3daea966bc35360d03e5fe5d66eeebb9028
kevin <kevin19960908@gmail.com>
2024-09-07 13:45:22 +0800

    側邊展開菜單

--- myfrontvite/src/components/horizontalMenu.vue
+++ myfrontvite/src/components/horizontalMenu.vue
@@ -1,45 +1,88 @@
 <script setup lang="ts">
 import { ref, computed, watch, onMounted, onBeforeUnmount } from 'vue'
+const props = defineProps<{ show?: boolean }>()
+
+watch(() => props.show, (newV) => {
+    toggleSecondLayer.value = newV;
+})
+
+const emit = defineEmits<{
+    (event: 'update:show', value: boolean): void
+}>();
+
 const firstLayerRef = ref<HTMLElement | null>(null);
-const secondLayerRect = ref<DOMRect | null>(null);
+const firstLayerRect = ref<DOMRect | null>(null);
+
 const updateRect = () => {
-    firstLayerRef.value && (secondLayerRect.value = firstLayerRef.value.getBoundingClientRect())
-    secondLayerRef.value && (thirdLayerRect.value = secondLayerRef.value.getBoundingClientRect())
+    firstLayerRef.value && (firstLayerRect.value = firstLayerRef.value.getBoundingClientRect())
+    secondLayerRef.value && (secondLayerRect.value = secondLayerRef.value.getBoundingClientRect())
 }
 
 const secondLayerRef = ref<HTMLElement | null>(null);
-const thirdLayerRect = ref<DOMRect | null>(null);
-const thirdLayerPosition = ref('end-0 translate-x-100%')
-const toggleThirdLayer = ref(false)
+const secondLayerRect = ref<DOMRect | null>(null);
+const secondLayerPosition = ref('end-0 translate-x-100%')
+const secondLayerHidden = ref('overflow-hidden')
+const secondLayerPadding = ref('')
+const toggleSecondLayer = ref(props.show || false)
 const handleThirdLayer = computed(() => {
-    if (!secondLayerRef.value?.textContent?.trim() || !toggleThirdLayer.value) return 'max-w-0 overflow-hidden'
-    return 'max-w-100% p-10px'
+    if (!secondLayerRef.value?.textContent?.trim() || !toggleSecondLayer.value) return 'max-w-0 overflow-hidden' + ' ' + secondLayerHidden.value + ' ' + secondLayerPadding.value
+    return 'max-w-100vw' + ' ' + secondLayerHidden.value + ' ' + secondLayerPadding.value
 })
-watch(() => thirdLayerRect.value, (newV, oldV) => {
-    if (thirdLayerRect.value) {
-        const { right, width } = thirdLayerRect.value;
+
+
+watch(() => secondLayerRect.value, (newV, oldV) => {
+    if (secondLayerRect.value) {
+        const { right, width } = secondLayerRect.value;
         const windowWidth = window.innerWidth;
-        if (windowWidth - right < 0 || (thirdLayerPosition.value == 'start-0 translate-x--100%' && (secondLayerRect.value && windowWidth - secondLayerRect.value.right < width))) {
-            thirdLayerPosition.value = 'start-0 translate-x--100%'
+        if (windowWidth - right < 0 || (secondLayerPosition.value == 'start-0 translate-x--100%' && (firstLayerRect.value && windowWidth - firstLayerRect.value.right < width))) {
+            secondLayerPosition.value = 'start-0 translate-x--100%'
             return
         }
-        thirdLayerPosition.value = 'end-0 translate-x-100%'
+        secondLayerPosition.value = 'end-0 translate-x-100%'
     }
 });
 
 
+const updateRectTrigger = () => {
+    setTimeout(() => {
+        if (toggleSecondLayer.value) {
+            updateRect()
+            updateRectTrigger()
+        }
+    }, 100)
+}
 
-setInterval(updateRect, 1000);
+watch(toggleSecondLayer, (newV) => {
+    if (newV) {
+        updateRectTrigger()
+        secondLayerPadding.value = 'p-10px'
+        setTimeout(() => {
+            secondLayerHidden.value = ''
+        }, 500);
+    }
+    else {
+        setTimeout(() => {
+            secondLayerPadding.value = ''
+        }, 500);
+        secondLayerHidden.value = 'overflow-hidden'
+    }
+})
+
+const toggle = () => {
+    const newValue = !toggleSecondLayer.value;
+    toggleSecondLayer.value = newValue;
+    emit('update:show', newValue);
+}
+setInterval(updateRect, 100);
 
 </script>
 <template>
-    <div ref="firstLayerRef" class="relative rounded-md bottom-0 duration-300">
-        <div class="cursor-pointer p-10px" @click="toggleThirdLayer = !toggleThirdLayer">
+    <div ref="firstLayerRef" class="relative">
+        <div class="cursor-pointer p-10px rounded-md bottom-0" @click="toggle">
             <slot name="title" />
         </div>
-        <div ref="secondLayerRef"
-            class="absolute shadow w-fit text-nowrap rounded-md top-0 box-border overflow-hidden duration-300"
-            :class="[thirdLayerPosition, handleThirdLayer]">
+        <div ref="secondLayerRef" class="absolute shadow text-nowrap rounded-md top-0 box-border ease-in"
+            :class="[secondLayerPosition, handleThirdLayer]" style="transition: max-width 500ms;">
             <slot />
         </div>
     </div>
