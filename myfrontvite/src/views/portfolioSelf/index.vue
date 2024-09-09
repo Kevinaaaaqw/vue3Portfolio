102a5fa6b11ecd5d77623479672b5e7d924427d6
kevin <kevin19960908@gmail.com>
2024-09-09 13:32:46 +0800

    切換作業平台

--- myfrontvite/src/views/portfolioSelf/index.vue
+++ myfrontvite/src/views/portfolioSelf/index.vue
@@ -4,14 +4,21 @@ import { ref, inject, computed, watch } from 'vue'
 <template>
     <div class="flex w-full items-center justify-center gradient h-100dvh font-noto">
         <div class="container flex justify-between">
-            <div class="w-300px aspect-[300/400] bg-slate_green_pale flex flex-col gap-4  items-center">
+            <div class="w-300px aspect-[300/400] bg-slate_green_pale flex flex-col gap-4 shadow items-center">
                 <img class="mt-40px w-50 object-cover aspect-square rounded-full object-[0px_0px]"
                     src="@/components/icons/1158433.jpg" alt="個人相片">
                 <div class="text-center font-bold text-2xl">黃凱群</div>
                 <div class="w-40px border-b-[3px] b-solid border-light_lavender"></div>
                 <div class="text-center">前端工程師</div>
                 <div class="flex-grow-1 w-full flex flex-col justify-end">
-                    <div class="bg-red">123</div>
+                    <div class="bg-beige_mist flex justify-center p-2">
+                        <a href="https://www.instagram.com/kevin19970908/" target="block">
+                            <i class="fa-brands fa-instagram fa-2x"></i>
+                        </a>
+                        <a href="https://www.linkedin.com/in/凱群-黃-8ba411304" target="block">
+                            <i class="fa-brands fa-linkedin fa-2x"></i>
+                        </a>
+                    </div>
                 </div>
             </div>
 
